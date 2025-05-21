55a590416a3d77f6a56c554a48db7b9e274a016e
Daniel Martinez <daniel.martinez5659@alumnos.udg.mx>
2025-05-21 01:29:16 -0600

    Todo App - First deploy

--- src/08-useReducer/intro-reducer.ts
+++ src/08-useReducer/intro-reducer.ts
@@ -1,35 +1,35 @@
-import type  {Todo, Action}  from "@/Types/Todo";
+// import type  {Todo, Action}  from "@/Types/Todo";
 
-const initialState:Todo[] = [{
-    id:     1,
-    description:   'Conquistar el mundo',
-    done:   false,
-}];
+// const initialState:Todo[] = [{
+//     id:     1,
+//     description:   'Conquistar el mundo',
+//     done:   false,
+// }];
 
 
-const todoReducer = ( state:Todo[] = initialState, action:Action ):Todo[] => {
+// const todoReducer = ( state:Todo[] = initialState, action:Action ):Todo[] => {
 
-    if (!action) return state;
+//     if (!action) return state;
 
-    switch (action.type.toLowerCase()) {
-        case 'add':
-            return [...state, action.payload];
+//     switch (action.type.toLowerCase()) {
+//         case 'add':
+//             return [...state, action.payload];
     
-        default:
-            console.log(`Action ${action.type} not recognized.`);
-            return state;
-    }
+//         default:
+//             console.log(`Action ${action.type} not recognized.`);
+//             return state;
+//     }
      
-}
-const newTodo = {
-    id:     2,
-    description:   'Destruir Puebla',
-    done:   false,
-}
+// }
+// const newTodo = {
+//     id:     2,
+//     description:   'Destruir Puebla',
+//     done:   false,
+// }
 
-const addTodo = {
-    type:    'add',
-    payload: newTodo,
-}
-let todos = todoReducer(initialState, addTodo);
-console.log(todos);
+// const addTodo = {
+//     type:    'ADD',
+//     payload: newTodo,
+// }
+// let todos = todoReducer(initialState, addTodo);
+// console.log(todos);
