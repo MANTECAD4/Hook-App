b6959e83cb31a8c7c48f4362c5e5c6c0f39350ff
Daniel Martinez <daniel.martinez5659@alumnos.udg.mx>
2025-05-21 12:10:53 -0600

    fix style.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import './08-useReducer/intro-reducer'
-import './index.css';
+import './style.css';
 import { TodoApp } from './08-useReducer/TodoApp';
 
 createRoot(document.getElementById('root')!).render(
