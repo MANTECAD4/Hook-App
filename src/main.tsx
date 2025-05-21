8fe0be66e1ec4a0d29fabf02243a365587a913ff
Daniel Martinez <daniel.martinez5659@alumnos.udg.mx>
2025-05-21 12:16:28 -0600

    Docs release lol

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import './08-useReducer/intro-reducer'
 import './style.css';
 import { TodoApp } from './08-useReducer/TodoApp';
 
